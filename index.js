1173d4399f62668b653df53aa960d048b90d0075
Tom-Leung-bb <wfleung8@cse.cuhk.edu.hk>
2021-06-29 11:08:08 +0800

    recaptcha

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const cors = require('cors');
 const multer = require('multer')
+const path = require('path')
+const fetch = require("node-fetch")
 const app = express()
 const port = 3001
+
+// enable body from req
+const bodyParser = require("body-parser")
+//json
+const jsonParser = bodyParser.json()
+//x-www-form-urlencoded
+const urlencodedParsser = bodyParser.urlencoded({extended: false})
+// use middle ware
+app.use(jsonParser)
+app.use(urlencodedParsser)
 
 app.use(cors());
 
@@ -16,7 +28,16 @@ const storage = multer.diskStorage({
   }
 })
 
-const upload = multer({ storage: storage }).single('file')
+const upload = multer({
+   storage: storage,
+   fileFilter: function (req, file, cb) {
+      const ext = path.extname(file.originalname);
+      if(ext !== '.png' && ext !== '.jpg'  && ext !== '.jpeg') {
+        return cb(new Error('Only images (jpg/png/jpeg) are allowed'))
+      }
+      cb(null, true)
+   } 
+  }).single('file')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -33,6 +54,17 @@ app.post("/upload", (req, res)=> {
     return res.status(200).send(req.file)
   })
 })
+
+app.post("/checkRecaptcha", (req, res)=> {
+  const {secret, response} = req.body
+  fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${response}`)
+  .then(response => response.json())
+  .then(data => {
+    res.send(data.success)
+  })
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
